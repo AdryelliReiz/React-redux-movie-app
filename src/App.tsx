7caa0327e65d893facbaed387585344538117088
Adryelli <adryellireis0santos@gmail.com>
2021-09-25 09:20:16 -0300

    create: routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Footer } from './components/Footer';
+
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+import { MovieDetails } from './components/MovieDetails';
+import { PageNotFound } from './components/PageNotFound/MovieListing';
+
+import './styles/app.scss'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/movie/:id" component={MovieDetails} />
+        <Route component={PageNotFound} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/PageNotFound/MovieListing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export const PageNotFound: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+}
