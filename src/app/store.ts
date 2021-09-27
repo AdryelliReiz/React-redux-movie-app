e531676fee5793a9cb0f59943a7fbda3e6611d1f
Adryelli <adryellireis0santos@gmail.com>
2021-09-27 20:04:30 -0300

    fix: store and moviesSlice types

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import movieReducer from "./features/movies/movieSlice";
+import moviesReducer from "./features/movies/moviesSlice";
 
 export const store = configureStore({
-  reducer: movieReducer,
+  reducer: {
+    movies: moviesReducer
+  },
 })
