876eceaffa50f85bad8921b701d1e88f437ebe07
Adryelli <adryellireis0santos@gmail.com>
2021-09-29 22:06:12 -0300

    fix: Listing of shows state

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { movieApi } from '../../api/movieApi';
-import { addMovies } from '../../app/features/movies/moviesSlice';
+import { fetchAsyncMovies, fetchAsyncShows } from '../../app/features/movies/moviesSlice';
 import { MovieListing } from '../MovieListing';
 import './styles.scss';
 
@@ -11,17 +10,9 @@ export const Home: React.FC = () => {
   const [textMovieSearch, setTextMovieSearch] = useState('Harry');
   
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${process.env.API_KEY || 'a558b683'}&s=${textMovieSearch}&type=movie`)
-        
-      const moviesData = response.data;
-
-      console.log(moviesData)
-      dispatch(addMovies(moviesData))
-    }
-    fetchMovies()
-  }, [])
+    dispatch(fetchAsyncMovies())
+    dispatch(fetchAsyncShows())
+  }, [dispatch])
 
   return (
     <>
