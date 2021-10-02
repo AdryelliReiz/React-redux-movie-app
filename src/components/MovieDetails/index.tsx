7a82aed5636ad1dcec1fd3b0e665802caf7ad9bf
Adryelli <adryellireis0santos@gmail.com>
2021-10-02 10:02:16 -0300

    fix: Loading component of movieDetails component

--- src/components/MovieDetails/index.tsx
+++ src/components/MovieDetails/index.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router';
 import { fetchAsyncMovieOrShowDetails, getSelectMovieOrShow, removeSelectedMoviesOrShows } from '../../app/features/movies/moviesSlice';
 
 import { FiStar, FiHeart, FiClock, FiCalendar } from "react-icons/fi";
+import ReactLoading from 'react-loading';
 import './styles.scss';
 
 export const MovieDetails: React.FC = () => {
@@ -20,51 +21,61 @@ export const MovieDetails: React.FC = () => {
   }, [dispatch, id])
   return (
     <div className="movie-section">
-      <div className="section-left">
-        <div className="movie-title">
-          {selectMovieOrShowData.Title}
-        </div>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating <i><FiStar size={24} /></i> : {selectMovieOrShowData.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <i><FiHeart size={24} /></i> : {selectMovieOrShowData.imdbVotes}
-          </span>
-          <span>
-            Runtime <i><FiClock size={24} /></i> : {selectMovieOrShowData.Runtime}
-          </span>
-          <span>
-            Year <i><FiCalendar size={24} /></i> : {selectMovieOrShowData.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{selectMovieOrShowData.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{selectMovieOrShowData.Director}</span>
+      {Object.keys(selectMovieOrShowData).length === 0
+        ? (
+          <ReactLoading type={'spin'} color="#79b8f3" />
+        )
+        : (
+          <>
+          <div className="section-left">
+            <div className="movie-title">
+              {selectMovieOrShowData.Title}
+            </div>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <i><FiStar size={24} /></i> : {selectMovieOrShowData.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <i><FiHeart size={24} /></i> : {selectMovieOrShowData.imdbVotes}
+              </span>
+              <span>
+                Runtime <i><FiClock size={24} /></i> : {selectMovieOrShowData.Runtime}
+              </span>
+              <span>
+                Year <i><FiCalendar size={24} /></i> : {selectMovieOrShowData.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{selectMovieOrShowData.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{selectMovieOrShowData.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{selectMovieOrShowData.Actors}</span>
+              </div>
+              <div>
+                <span>Generes</span>
+                <span>{selectMovieOrShowData.Genre}</span>
+              </div>
+              <div>
+                <span>Languages</span>
+                <span>{selectMovieOrShowData.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{selectMovieOrShowData.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{selectMovieOrShowData.Actors}</span>
+          <div className="section-right">
+            <img src={selectMovieOrShowData.Poster} alt={selectMovieOrShowData.Title} />
           </div>
-          <div>
-            <span>Generes</span>
-            <span>{selectMovieOrShowData.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{selectMovieOrShowData.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{selectMovieOrShowData.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={selectMovieOrShowData.Poster} alt={selectMovieOrShowData.Title} />
-      </div>
+          </>
+        )
+      }
+      
     </div>
   );
 }
